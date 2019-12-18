e7a756eaf53d5ad10a02912145a85685e4187107
suren <savetis.dev@gmail.com>
2019-12-18 13:30:12 +0400

    add StockBlock component for displaying stocks

---
+++ src/components/StockBlock.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="rounded overflow-hidden shadow-lg border border-gray-400 mb-8 flex flex-col">
+    <div class="flex justify-between">
+      <span class="text-gray-900 font-bold p-3">{{ title }}</span>
+      <span class="flex items-center py-2">
+        <input
+          class="appearance-none bg-gray-200 border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          type="text"
+          placeholder="Stock Name"
+        />
+      </span>
+    </div>
+    <div class="border-t border-gray-400 p-3 flex flex-wrap">
+      <span
+        class="p-2 mr-2 mb-2 border border-orange-400 cursor-pointer"
+        v-for="item in items"
+        :key="item.id"
+      >
+        {{ item.name }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "items"]
+};
+</script>
--- src/components/StockPage.vue
+++ src/components/StockPage.vue
@@ -1,27 +1,22 @@
 <template>
   <main class="w-full flex flex-col items-center p-6">
     <div
-      class="max-w-sm rounded overflow-hidden shadow-lg border border-gray-400 mb-8 w-3/4 flex flex-col"
+      class="max-w-sm w-3/4"
       v-for="stockGroup in tradeBox"
       :key="stockGroup.id"
     >
-      <span class="text-gray-900 font-bold p-3">{{ stockGroup.title }}</span>
-      <div class="border-t border-gray-400 p-3 flex flex-wrap">
-        <span
-          class="p-2 mr-2 mb-2 border border-orange-400 cursor-pointer"
-          v-for="item in stockGroup.stocks"
-          :key="item.id"
-        >
-          {{ item.name }}
-        </span>
-      </div>
+      <stock-block
+        :title="stockGroup.title"
+        :items="stockGroup.stocks"
+      />
     </div>
   </main>
 </template>
 
 <script>
-import gql from 'graphql-tag';
-import { useQuery, useResult } from '@vue/apollo-composable';
+import gql from "graphql-tag";
+import { useQuery, useResult } from "@vue/apollo-composable";
+import StockBlock from "./StockBlock";
 
 const GET_BOXES = gql`
   query getBoxes {
@@ -35,6 +30,9 @@ const GET_BOXES = gql`
 `;
 
 export default {
+  components: {
+    StockBlock
+  },
   setup() {
     const { result } = useQuery(GET_BOXES);
 
